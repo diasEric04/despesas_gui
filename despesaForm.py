79e870d89332478e823a0454a563401c50ba23d0
Eric Arruda Dias <ericarrudadias@gmail.com>
2023-06-28 17:22:40 -0300

    começando a configurar o formulario de despesas

--- despesaForm.py
+++ despesaForm.py
@@ -1,6 +1,9 @@
-from PySide6.QtWidgets import QGridLayout
+from PySide6.QtWidgets import QGridLayout, QWidget
 
 
 class DespesaForm(QGridLayout):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
+
+    def addWidgetToForm(self, widget: QWidget, *args, **kwargs):
+        self.addWidget(widget, *args, **kwargs)
--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from PySide6.QtWidgets import QApplication, QPushButton
 from mainWindow import MainWindow
 import data
 from styles import setupTheme
+from despesaForm import DespesaForm
 
 if __name__ == '__main__':
     DATA_FOLDER = data.initData()
@@ -10,9 +11,21 @@ if __name__ == '__main__':
     window = MainWindow()
     setupTheme(DATA_FOLDER)
 
-    buttonTeste = QPushButton('botao de teste')
-    buttonTeste.clicked.connect(lambda: print('botao clicado'))
-    window.addWidgetToLayout(buttonTeste)
+    despesaForm = DespesaForm()
+    window.vLayout.addLayout(despesaForm)
+
+    button1 = QPushButton('botao de teste 1')
+    button1.clicked.connect(lambda: print('botao 1 clicado'))
+
+    button2 = QPushButton('botao de teste 2')
+    button2.clicked.connect(lambda: print('botao 2 clicado'))
+
+    button3 = QPushButton('botao de teste 3')
+    button3.clicked.connect(lambda: print('botao 3 clicado'))
+
+    despesaForm.addWidgetToForm(button1, 0, 0)
+    despesaForm.addWidgetToForm(button2, 0, 1)
+    despesaForm.addWidgetToForm(button3, 1, 0, 1, 2)
 
     window.show()
     app.exec()
